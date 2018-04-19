ea9611c19bdf5a4cf3631b883e6e7e14aa3d10f2
HaaZee <haazee4droid@gmail.com>
2018-04-19 19:13:09 +0200

    Némi refactoring végrehajtása a measure.py-n. Szétszedtem - igaz, elég ocsmányul - három önálló funkcióra a spagettit. Lett egy konfig olvasó, betöltő, ellenőrző, lett egy teszteket futtató és lett egy az eredményeket kiíró függvényem. Nem kicsit ronda. Lehet, hogy átírom valami OOP like formára... :)

--- measure.py
+++ measure.py
@@ -7,13 +7,14 @@ import subprocess
 import shlex
 import shutil
 import json
+import traceback
 from collections import OrderedDict
 from typing import List, Any
 
 PARAMETER_FILE = 'measure.json'  # type: str
 
 
-def run_tests():
+def load_params_from_json(file_name):
     # Paraméter fájl: measure.json
     # Formátum:
     # { "repeat": <n>, "tests": [["program", "paraméter", "elnevezés"], ...] }
@@ -24,7 +25,7 @@ def run_tests():
     #        tartalma is, csak rondának találtam a ./speed_test formát.
     tests = {}
     try:
-        with open(PARAMETER_FILE, 'r') as conf_file:
+        with open(file_name, 'r') as conf_file:
             config_data = json.load(conf_file, object_pairs_hook=OrderedDict)
 
         for (prog, param, name) in config_data["tests"]:
@@ -36,13 +37,20 @@ def run_tests():
                 tests[name] = [cmd, []]
             else:
                 print("{} is missing or not executable".format(prog), file=sys.stderr)
-
-    except Exception as e:
-        print(type(e), file=sys.stderr)
-        print(e, file=sys.stderr)
+    except Exception:
+        traceback.print_exc()
         sys.exit(-1)
+    return config_data["repeat"], tests
 
-    for i in range(config_data["repeat"]):
+
+def run_tests(p_repeat, p_tests):
+    # Csúnyán működik egyelőre, mert a p_tests "cím szerint" adódik át, én meg ezt egyszerre
+    # használom inputként és outputként
+    # Megtehetném, hogy visszaadom, mint visszatérési értéket, de azt itt még rondábbnak érzem.
+    #
+    tests = p_tests
+    print("tests == {}".format(tests))
+    for i in range(p_repeat):
         print("Round: {}".format(i+1))
         faulty_tests = []  # type: List[Any]
         for test_name in tests.keys():
@@ -69,21 +77,25 @@ def run_tests():
             del tests[test_name]
             print("Faulty test {} deleted".format(test_name), file=sys.stderr)
 
-    for test_name in tests:
+
+def print_results(p_tests):
+    for test_name in p_tests:
         print("\n\n")
         print("Test: {}".format(test_name))
         summ_ut = 0
         summ_st = 0
-        for results in tests[test_name][1]:
+        for results in p_tests[test_name][1]:
             print("    User: {}  System: {}".format(results.ru_utime, results.ru_stime))
             summ_ut += results.ru_utime
             summ_st += results.ru_stime
 
-        avg_ut = summ_ut/len(tests[test_name][1])
-        avg_st = summ_st/len(tests[test_name][1])
+        avg_ut = summ_ut/len(p_tests[test_name][1])
+        avg_st = summ_st/len(p_tests[test_name][1])
         print("    U.avg: {}   S.avg: {}    Total avg: {}".format(avg_ut, avg_st, avg_ut+avg_st))
 
 
 if __name__ == "__main__":
-    run_tests()
+    (repeat_counter, wtests) = load_params_from_json(PARAMETER_FILE)
+    run_tests(repeat_counter, wtests)
+    print_results(wtests)
     sys.exit(0)
